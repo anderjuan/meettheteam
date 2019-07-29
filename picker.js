a8a7d761c1810523ae9541d01ffaf28870257d2b
Anderjuan <48619904+anderjuan@users.noreply.github.com>
2019-07-29 20:47:01 +0200

    Added avatars and people

--- picker.js
+++ picker.js
@@ -8,14 +8,14 @@ function clean() {
 } //Cleans description panel
 
 function getInfo(name) {switch (name) {
-  case 'César':
-    document.getElementById('description').innerHTML = `<span class="name">Hello, my name is <br> ${name}</span><br>` + '<span class="badge">Foodie</span>' + '<p class="bio es">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
+  case 'Cesar':
+    document.getElementById('description').innerHTML = `<span class="name">Hello, my name is <br> ${name}</span><br>` + '<span class="badge">Sport, Foodie</span>' + '<p class="bio es">Si quieres convertirte en un cliente asiduo habla con el director. Para cualquier sugerencia o recomendación del barrio seguro te sorprenderá.</p>';
     break;
-  case 'María':
-    document.getElementById('description').innerHTML = `<span class="name">Hello, my name is <br> ${name}</span><br>` + '<span class="badge">Night Owl</span>' + '<p class="bio es">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
+  case 'Esther':
+    document.getElementById('description').innerHTML = `<span class="name">Hello, my name is <br> ${name}</span><br>` + '<span class="badge">Beauty, Foodie</span>' + '<p class="bio es">Conozco perfectamente el barrio, aunque especialmente todo lo relacionado con “beauty”, salones de belleza, peluquería…  </p><p class="bio es">Además como jefa de recepción, puedes contactarme para conseguir las mejores tarifas, tipo de habitación…</p>';
     break;
   default:
-    document.getElementById('description').innerHTML =  `oopsie, no ${name} found`
+    document.getElementById('description').innerHTML =  `<span class="name">oopsie, no ${name} found</span>`
     break;
 }
 } //Prints the description for name
